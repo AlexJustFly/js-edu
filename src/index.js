a031ac5256a6725d6962bcb9a0889a3f65b7fa53
AlexJustFly <alexjustfly@gmail.com>
2019-09-28 23:03:17 +0300

    -u

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+  let result = knowsProgramming ? 800/config[focus] : 1300/config[focus];
+      return Math.ceil(result);
   };
   
